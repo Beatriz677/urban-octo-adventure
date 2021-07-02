55cfddc2404b997de7fa26e23582cd3bf42df641
Beatriz677 <86676818+Beatriz677@users.noreply.github.com>
2021-07-01 19:50:43 -0500

    Add files via upload

---
+++ Naming.playground/Pages/11-ExerciseiPhone.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+let totalOfMB = 1000 * 8
+
+let totalOfGBOnPhone = 8000 - 3000
+
+// Total of GB
+let totalOfGB = 5000 - 150 * 33
+
+
+
+
+
+//:[Previous](@previous)  |  page 12 of 14  |  [Next: Exercise: Fixing Your Morning](@next)
+
